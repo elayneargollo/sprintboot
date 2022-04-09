bcf58f2d85497de57722402b543bc6e0205d9678
Elayne Natália <elayne.argollo@solutis.com.br>
2022-04-09 01:49:20 -0300

    collection postaman

--- src/main/java/br/com/solutis/votacao/controller/AssociadoController.java
+++ src/main/java/br/com/solutis/votacao/controller/AssociadoController.java
@@ -2,7 +2,6 @@ package br.com.solutis.votacao.controller;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
